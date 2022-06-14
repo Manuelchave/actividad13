61874e23300039c3bd38fc6713aa18a433490ad0
unknown <pelomanuel@gmail.com>
2022-06-13 18:29:09 -0600

    first commit

---
+++ src/components/ProfileScreen.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+
+
+const ProfileScreen = ({ navigation }) => {
+    return (
+      <Button
+        title="Go to Jane's profile"
+        onPress={() =>
+          navigation.navigate('Profile', { name: 'Jane' })
+        }
+      />
+    );
+  };
+  
+
+export default ProfileScreen
